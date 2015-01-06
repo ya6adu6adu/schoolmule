6f51249ee4b868e823b1f3c853f497111ad9235a
Maxim Minchenkov <mminchenkov@xbsoftware.com>
2015-01-06 18:46:04 +0300

    test

---
+++ setup.php
@@ -0,0 +1,12 @@
+<?php
+
+
+	$params = array(
+			'controls' => array('tabs','layout','grid','html','tree','window','accordeon'),
+			'instances' => array(/*'grid_import_dbstructure','grid_import_drag','grid_import_fields',*/'assign_pupils_window','tree_export','grid_export','grid_search','grid_setup_parents','grid_setup_database','tree_users','tree_programme_structure','grid_setup_pupils','grid_setup_staff','grid_setup_studygroups','grid_setup_pupils_stg','window_setup_tree'),
+    		'title' => "browser_tab_setup",
+    		'top_title' => '',
+			'name' => 'setup'
+    	);
+
+    	include_once '__pages.php';
